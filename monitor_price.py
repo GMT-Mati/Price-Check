40522d1a1e36ae9ccd1cd6cfb9a49a1dd5c33ce1
Mateusz Gruszka <gruszka.mateusz87@gmail.com>
2023-09-24 13:18:39 +0200

    add euro

--- monitor_price.py
+++ monitor_price.py
@@ -1,57 +1,74 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-import time
 
-# Define the URL of the product page you want to monitor
-product_url = "https://example.com/product"
+def send_email(items):
+    # Set up your email configuration
+    sender_email = "your_mail@gmail.com"
+    sender_password = "password"
+    recipient_email = "example_email@gmail.com"
 
-# Set the price threshold below which you want to be notified
-target_price = 100.00  # Change this to your desired price
-
-# Email configuration (Gmail in this example)
-sender_email = "youremail@gmail.com"
-sender_password = "yourpassword"
-receiver_email = "recipient@example.com"
-
-# Function to send an email notification
-def send_email(price, product_url):
     subject = "Price Alert"
-    body = f"The price of the product is now ${price:.2f}.\n\nCheck it out here: {product_url}"
+    message = "\n".join(items)
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(sender_email, sender_password)
-        message = f"Subject: {subject}\n\n{body}"
-        server.sendmail(sender_email, receiver_email, message)
-        print("Email notification sent!")
-        server.quit()
+
+        email_text = f"Subject: {subject}\n\n{message}"
+        server.sendmail(sender_email, recipient_email, email_text)
+        print("Email notification sent successfully!")
     except Exception as e:
-        print("Error sending email:", str(e))
+        print("An error occurred while sending the email:", str(e))
+    finally:
+        server.quit()
 
-# Main function to check the price and send notifications
-def monitor_price():
-    while True:
+def price_check(URLs, headers):
+    items_to_notify = []
+
+    for URL, price_threshold in URLs:
         try:
-            # Send a GET request to the product URL
-            response = requests.get(product_url)
-
-            # Parse the HTML content
-            soup = BeautifulSoup(response.text, 'html.parser')
-
-            # Extract the price from the page (you may need to inspect the webpage HTML)
-            # Replace this with the appropriate code to extract the price
-            current_price = float(soup.find("span", {"class": "product-price"}).text.replace("$", ""))
-
-            # Check if the current price is lower than the target price
-            if current_price < target_price:
-                send_email(current_price, product_url)
-
-            # Sleep for an hour (you can adjust the interval)
-            time.sleep(3600)
+            page = requests.get(URL, headers=headers)
+            soup = BeautifulSoup(page.content, "html.parser")
+            
+            if "notino" in URL:
+                price_element = soup.find(id="pd-price")
+                if price_element:
+                    price = price_element.get_text()
+                    # Extract numerical value from the price string
+                    price_value = float("".join(filter(str.isdigit, price[0:3])))
+                    if price_value < price_threshold:
+                        items_to_notify.append(f"Item: {URL}\nPrice: {price_value}")
+                    print("Price:", price_value)
+                else:
+                    print("Price not found on the page.")
+            
+            elif "euro.com.pl" in URL:
+                price_element = soup.find("span", class_="price-template__default--amount")
+                if price_element:
+                    price = price_element.get_text().strip()
+                    # Remove currency symbols or spaces, if present
+                    price_value = float("".join(filter(str.isdigit, price)))
+                    if price_value < price_threshold:
+                        items_to_notify.append(f"Item: {URL}\nPrice: {price_value}")
+                    print("Price:", price_value)
+                else:
+                    print("Price not found on the page.")
         except Exception as e:
-            print("Error:", str(e))
+            print("An error occurred:", str(e))
+    
+    if items_to_notify:
+        send_email(items_to_notify)
 
-if __name__ == "__main__":
-    monitor_price()
+# Example usage:
+# you can replace  url adress with any adress from www.notino.pl and www.euro.com.pl, also you can change price for any value you want
+URLs = [
+    ("https://www.notino.pl/azzaro/chrome-woda-toaletowa-dla-mczyzn/p-59969/", 300),
+    ("https://www.notino.pl/guerlain/aqua-allegoria-pera-granita-woda-toaletowa-dla-kobiet/", 400),
+    ("https://www.euro.com.pl/odkurzacze-automatyczne/roborock-s8-czarny-mopowanie.bhtml?cd=191780169&ad=10070947089&kd=&gclid=EAIaIQobChMI3o7gg7XAgQMV2OyyCh0vZQ5aEAQYBCABEgLcOvD_BwE&gclsrc=aw.ds", 2500),
+    ("https://www.euro.com.pl/odkurzacze-automatyczne/roborock-q7-max-bialy.bhtml?&cd=191780169&ad=10070947089&kd=&gclid=Cj0KCQjwvL-oBhCxARIsAHkOiu0R-ybG2gKqgA2JwP9MWxqIoeNi60vhT709nlSer7e6CMqssst_084aAmyKEALw_wcB&gclsrc=aw.ds", 2000)
+]
+
+headers = {"User-Agent": "Your User Agent"}
+price_check(URLs, headers)
